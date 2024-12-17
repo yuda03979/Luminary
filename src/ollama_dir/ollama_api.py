684ee26a7b74c8aac4f0855e27b704694c298657
= <=>
2024-12-17 01:58:24 +0000

    chat

--- src/ollama_dir/ollama_api.py
+++ src/ollama_dir/ollama_api.py
@@ -1,11 +1,32 @@
 from typing import Union, Sequence
 import ollama
-
+from tqdm import tqdm
 
 class OllamaApi:
 
     def __init__(self, model_name):
         self.model_name = model_name
+        self.pull()
+    
+    def pull(self):
+
+        current_digest, bars = '', {}
+        for progress in ollama.pull(self.model_name, stream=True):
+            digest = progress.get('digest', '')
+            if digest != current_digest and current_digest in bars:
+                bars[current_digest].close()
+
+            if not digest:
+                print(progress.get('status'))
+                continue
+
+            if digest not in bars and (total := progress.get('total')):
+                bars[digest] = tqdm(total=total, desc=f'pulling {digest[7:19]}', unit='B', unit_scale=True)
+
+            if completed := progress.get('completed'):
+                bars[digest].update(completed - bars[digest].n)
+
+        current_digest = digest
 
 
     def chat_stream(self, messages: list):
--- src/utils/globals.py
+++ src/utils/globals.py
@@ -1,6 +1,6 @@
 class Globals:
 
-    model_name = "llama3.2:1b"
+    model_name = ["vicuna:7b-q2_K", "gemma2:2b", "llama3.2:1b"][1]
 
     def __init__(self):
         pass
